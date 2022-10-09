9e74c80cdca0d969445e565171edfd4368928b96
Aditya Sidharta <aditya.sdrt@gmail.com>
2022-10-08 22:09:43 -0400

    [Adi] Finish project 1

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -51,6 +51,9 @@ $(document).ready(function() {
       .then((response) => {
         console.log(response);
         var data = response.data;
+        console.log(data);
+        console.log(data.messages);
+        console.log(data.messages.length > 0);
 
         if (data.messages && data.messages.length > 0) {
           console.log('received ' + data.messages.length + ' messages');
