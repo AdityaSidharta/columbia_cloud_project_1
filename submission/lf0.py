36ad4b3759584d56435b8faa9a44176ba692c595
Aditya Sidharta <aditya.sdrt@gmail.com>
2022-10-08 22:16:20 -0400

    [Adi] :sparkles: Add submission

---
+++ submission/lf0.py
@@ -0,0 +1,91 @@
+import boto3
+import uuid
+import datetime
+
+lex = boto3.client('lexv2-runtime')
+sqs = boto3.client('sqs')
+
+botID = '4MWEOFDHPK'
+botAliasID = 'XQHOMXFVAA'
+sessionID = 'testuser'
+localeID = 'en_US'
+queue_url = 'https://sqs.us-east-1.amazonaws.com/139100004146/Proj1SQS'
+
+
+def lambda_handler(event, context):
+    incoming_msgs = event['messages']
+    assert len(incoming_msgs) == 1
+    incoming_msg = incoming_msgs[0]
+    print("incoming_msg : {}".format(incoming_msg))
+    incoming_msg_content = incoming_msg['unstructured']['text']
+    
+    # Initiate conversation with Lex
+    response = lex.recognize_text(
+            botId=botID,
+            botAliasId=botAliasID,
+            localeId=localeID,
+            sessionId=sessionID,
+            text=incoming_msg_content)
+    
+    outgoing_msgs = response['messages']
+    session_intent = response['sessionState']['intent']
+    
+    if (session_intent['name'] == 'DiningSuggestionsIntent') & (session_intent['state'] == 'ReadyForFulfillment') & (session_intent['confirmationState'] == 'Confirmed'):
+        slots = session_intent['slots']
+        result = {k: v['value']['interpretedValue'] for k, v in slots.items()}
+        print("Sending result to SQS : {}".format(result))
+
+        response = sqs.send_message(
+              QueueUrl=queue_url,
+              DelaySeconds=10,
+              MessageAttributes={
+                  'Cuisine': {
+                      'DataType': 'String',
+                      'StringValue': result['Cuisine']
+                  },
+                  'Date': {
+                      'DataType': 'String',
+                      'StringValue': result['Date']
+                  },
+                  'Email': {
+                      'DataType': 'String',
+                      'StringValue': result['Email']
+                  },
+                  'Location': {
+                      'DataType': 'String',
+                      'StringValue': result['Location']
+                  },
+                  'PeopleNumber': {
+                      'DataType': 'String',
+                      'StringValue': result['PeopleNumber']
+                  },
+                  'PhoneNum': {
+                      'DataType': 'String',
+                      'StringValue': result['PhoneNum']
+                  },
+                  'Time': {
+                      'DataType': 'String',
+                      'StringValue': result['Time']
+                  }
+              },
+              MessageBody=str(uuid.uuid4())
+          )
+    
+    if len(outgoing_msgs):
+        msgs = []
+        for outgoing_msg in outgoing_msgs:
+            print("outgoing_msg : {}".format(outgoing_msg))
+            outgoing_msg_content = outgoing_msg['content']
+            msgs.append({
+                    "type": "unstructured",
+                    "unstructured": {
+                    "id": str(uuid.uuid4()),
+                    "text": outgoing_msg_content,
+                    "timestamp": datetime.datetime.isoformat(datetime.datetime.now()),
+                    }
+                })
+        return {
+                "messages": msgs
+            }
+    else:
+        raise ValueError("Lex has Failed")
---
+++ submission/lf2.py
@@ -0,0 +1,135 @@
+def lambda_handler(event, context):
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName=queue_name)
+    messages = queue.receive_messages(MessageAttributeNames=['All'])
+    if len(messages):
+        for message in messages:
+            print("Processing message {}".format(message.message_id))
+
+            cuisine = message.message_attributes.get('Cuisine').get('StringValue')
+            date = message.message_attributes.get('Date').get('StringValue')
+            email = message.message_attributes.get('Email').get('StringValue')
+            people_number = message.message_attributes.get('PeopleNumber').get('StringValue')
+            booking_time = message.message_attributes.get('Time').get('StringValue')
+
+            print("Receiving message from SQS - Cuisine : {}, Date : {}, Email : {}, People Number : {}, Booking Time : {}".format(
+                cuisine,
+                date,
+                email,
+                people_number,
+                booking_time
+            ))
+
+            credentials = boto3.Session().get_credentials()
+            auth = AWSV4SignerAuth(credentials, region)
+            opensearch = OpenSearch(
+                hosts = [{'host': opensearch_host, 'port': port}],
+                http_auth = auth,
+                use_ssl = True,
+                verify_certs = True,
+                connection_class = RequestsHttpConnection
+            )
+
+            query = {
+            'size': size,
+            'query': {
+                'multi_match': {
+                'query': cuisine,
+                'fields': ['cuisine']
+                }
+            }
+            }
+
+            response = opensearch.search(
+                body = query,
+                index = index_name
+            )
+
+            if response['hits']:
+                restaurant_ids = [hits['_source']['restaurant_id'] for hits in response['hits']['hits']]
+                print("Receiving restaurant_ids from OpenSearch : {}".format(restaurant_ids))
+            else:
+                raise ValueError("Open Search Failed")
+
+            dynamodb = boto3.client("dynamodb")
+
+            locations = []
+            names = []
+
+            assert len(restaurant_ids)
+
+            for restaurant_id in restaurant_ids:
+
+                response = dynamodb.get_item(
+                    TableName='cloud1-db',
+                    Key={
+                        'id': {'S': restaurant_id},
+                    }
+                )
+
+                try:
+                    location = response['Item']['location']['S']
+                    name = response['Item']['name']['S']
+                    locations.append(location)
+                    names.append(name)
+                    print("restaurant_id : {}".format(restaurant_id))
+                    print("name : {}".format(name))
+                    print("location : {}".format(location))
+                except:
+                    raise ValueError("DynamoDB Failed")
+
+            sns = boto3.client("sns")
+            response = sns.list_topics()
+            topic_arn = response["Topics"][0]['TopicArn']
+            print("Subscribing email : {}".format(email))
+            response = sns.subscribe(TopicArn=topic_arn, Protocol="email", Endpoint=email)
+            subscription_arn = response["SubscriptionArn"]
+
+            timeout = 0
+            while subscription_arn == 'pending confirmation':
+                if timeout == max_timeout:
+                    raise ValueError("Email is not confirmed : {}. subscription_arn : {}".format(email, subscription_arn))
+                else:
+                    print("Email {} has not been confirmed. subscription_arn : {}. Sleeping...".format(email, subscription_arn))
+                    time.sleep(sleep_time)
+                    timeout = timeout + 1
+                    subscription_arn = [x['SubscriptionArn'] for x in sns.list_subscriptions()['Subscriptions'] if x['Endpoint'] == email][0]
+
+            print("email {} subscription_arn has been confirmed : {}".format(email, subscription_arn))
+
+            subject = "Message from DiningConcierge"
+            message_body = """Hello! Here are my {cuisine} restaurant suggestions for {people_number} people, for {date} at {booking_time}: 1. {name0}, located at {location0}, 2. {name1}, located at {location1}, 3. {name2}, located at {location2}. Enjoy your meal!""".format(
+                cuisine= cuisine,
+                people_number= people_number,
+                date= date,
+                booking_time= booking_time,
+                name0= names[0],
+                location0= locations[0],
+                name1= names[1],
+                location1= locations[1],
+                name2= names[2],
+                location2= locations[2]
+            )
+
+            message_id = sns.publish(TopicArn=topic_arn, 
+                    Message=message_body, 
+                    Subject=subject)['MessageId']
+            print("Message has been sent to {}".format(email))
+            message.delete()
+            print("Message has been deleted from SQS")
+
+            return {
+                'cuisine' : cuisine,
+                'date' : date,
+                'email' : email,
+                'people_number' : people_number,
+                'booking_time' : booking_time,
+                'restaurant_ids': restaurant_ids,
+                'names': names,
+                'locations': locations,
+                'subject': subject,
+                'message': message,
+                'message_id': message_id
+            }
+    else:
+        print("No messages found!")
